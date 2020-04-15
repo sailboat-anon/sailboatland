1f1a58cb267f6b174d343970de33f7c02e3218de
sailboatAnon <devnull>
2020-04-15 16:07:48 -0700

    no message

--- index.php
+++ index.php
@@ -15,6 +15,30 @@ echo "<pre>
 
       https://github.com/sailboat-anon/sailboatland
 
+*** TUTORIAL
+
+URL (HTTPS-only)
+api.cyberland2.club:443
+
+*** POSTING
+This will create a post to the off topic board containing the content x and replying to y, if y is unspecified, then it will be considered that it does not reply to anything.
+curl https://api.cyberland2.club/o/?content=x&replyTo=y
+
+*** GETTING POSTS
+This will get y number of posts from the off topic board that reply to post number x with the newest first as a JSON object. If x is unspecified, just y number of recent posts will be returned. The number of posts you can recieve at once may be limited at some point depending on how this goes.
+
+curl https://api.cyberland2.club/o/?thread=x&num=y
+
+You can sort using sortOrder (values: "bumpCount", "time", "id") in ascending or descending order using sortHierarchy (values: "ASC", "DESC").
+
+*** SORTING
+If I wanted to get all posts on /t/, sorted by bumpcount I would do this:
+curl https://api.cyberland2.club/t/?sortOrder=bumpCount
+
+This would get you all the oldest posts first:
+curl https://api.cyberland2.club/t/?sortOrder=time&sortHierarchy=ASC
+
+
                 ,_
                 I~
                 |\
