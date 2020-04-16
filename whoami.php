609d77a4ee9830c04a35b4ab09d9e72f54426575
sailboatAnon <devnull>
2020-04-15 18:55:41 -0700

    nginx doesn't support getallheaders();

--- whoami.php
+++ whoami.php
@@ -3,9 +3,8 @@
 function get(): void
 {
     header('Content-Type: application/json');
-    $hdrs = getallheaders();
-    $anon = sha1($hdrs["Host"]);
-    $client = $hdrs["User-Agent"];
+    $anon = sha1($_SERVER['REMOTE_ADDR']);
+    $client = $_SERVER['HTTP_USER_AGENT'];
     $out = array($anon, $client);
     echo(json_encode($out));
 }
