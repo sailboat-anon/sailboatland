7bfd714b8c9b2df56a29ab041b9e592d7da09094
sailboatAnon <devnull>
2020-04-15 20:31:18 -0700

    ipinfo.io stuff

--- whoami.php
+++ whoami.php
@@ -5,6 +5,15 @@ function get(): void
     header('Content-Type: application/json');
     $anon = sha1($_SERVER['REMOTE_ADDR']);
     $client = $_SERVER['HTTP_USER_AGENT'];
-    $out = array($anon, $client);
+    $ch = curl_init();
+    $url = "http://ipinfo.io/";
+    $info = gethostbyname($_SERVER['REMOTE_ADDR']);
+
+    curl_setopt($ch, CURLOPT_URL, $url.$info);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = json_encode(curl_exec($ch));
+    curl_close($ch); 
+
+    $out = array($anon, $client, $output);
     echo(json_encode($out));
 }
