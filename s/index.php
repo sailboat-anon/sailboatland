8baac74d22c844295d387b7db78e93f199e99ebb
sailboatAnon <devnull>
2020-04-23 17:59:48 -0700

    Added federated user/pass to db.conf

--- s/index.php
+++ s/index.php
@@ -1,6 +1,9 @@
 <?php
 namespace sailboats;
-$endpoint = 'https://api.cyberland2.club/s/';
+$endpoint = 'https://api.cyberland2.club';
+$db_config = parse_ini_file(__DIR__ . 'config/db.conf');
+$federatedKey = $db_config['federatedKey'];
+$federatedUser = $db_config['federatedUser'];
 
 $sb = new sharedBoard();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -16,7 +19,7 @@ class sharedBoard {
         $limit = intval($_GET['num'] ?? 50);  if ($limit > 50) { $limit = 50; }
         $thread = intval($thread ?? 0);
 
-        $url = $endpoint . '?replyTo=' . $thread . '&num=' . $limit;
+        $url = $endpoint . '/s/?replyTo=' . $thread . '&num=' . $limit;
         $c = curl_init();
         curl_setopt($c, CURLOPT_URL, $url);
         echo(json_encode(curl_exec($c)));
@@ -24,7 +27,7 @@ class sharedBoard {
     }
 
 	function post() {
-        global $endpoint;
+        global $endpoint, $federatedUser, $federatedKey;
         $thread = $_POST['replyTo'] ?? $_POST['thread'] ?? 0;
         $thread = intval($thread ?? 0);
         if(!isset($_POST['content'])) { echo 'Err: add content to sharedboard post.'; exit; }
@@ -33,9 +36,9 @@ class sharedBoard {
             'replyTo'   =>  $thread,
             'content'   =>  $_POST['content'],
         );
-        
+        $url = $endpoint . '/s/';
         $c = curl_init();
-        curl_setopt($c, CURLOPT_URL, $endpoint);
+        curl_setopt($c, CURLOPT_URL, $url);
         curl_setopt($c, CURLOPT_POST, TRUE);
         curl_setopt($c, CURLOPT_POSTFIELDS, $payload);
         curl_exec($c);
@@ -44,11 +47,32 @@ class sharedBoard {
 
         switch ($httpcode) {
             case 200:
-                header("HTTP/1.1 200 OK", TRUE, 429);
+                header('HTTP/1.1 200 OK', TRUE, 429);
                 break;
             case 401:
                 header('HTTP/1.1 401 Unauthorized', TRUE, 401); 
-                // get a new JWT from api.cyberland2.club
+                function() {
+                    global $endpoint;
+                    //api.cyberland2.club/api/v1/auth -d 'username=sba&password=ycLPyE#cLL$wu8WrJ)R~86REecn'
+                    $auth_payload = array(
+                        'username'  =>  $federatedUser,
+                        'password'  =>  $federatedKey,
+                    );
+                    $c = curl_init();
+                    $url = $endpoint . '/api/v1/auth/';
+                    curl_setopt($c, CURLOPT_URL, $endpoint);
+                    curl_setopt($c, CURLOPT_POST, TRUE);
+                    curl_setopt($c, CURLOPT_POSTFIELDS, $auth_payload);
+                    curl_exec($c);
+                    $httpc = curl_getinfo($c, CURLINFO_HTTP_CODE);
+                    curl_close($c);
+                    if ($httpc == 200) { // re-post/get
+                        $this->post();
+                    } elseif ($httpc == 401) {
+                        echo "Cyberland Server Error: You do not have an authorized account with api.cyberland2.com.  Contact sailboatanon@protonmail DOT com to get federated credentials.";
+                        exit;
+                    } else { header('HTTP/1.1 $httpcode', TRUE, $httpcode); }
+                };
                 break;
             case 404:
                 header('HTTP/1.1 404 Not Found', TRUE, 404);
