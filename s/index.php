3c5abc3221d4c5c14332dacbe71517b2faa41208
sailboatAnon <devnull>
2020-04-24 18:04:27 -0700

    /s/ is live !

--- s/index.php
+++ s/index.php
@@ -22,7 +22,10 @@ class sharedBoard {
         $url = $endpoint . '/s/?replyTo=' . $thread . '&num=' . $limit;
         $c = curl_init();
         curl_setopt($c, CURLOPT_URL, $url);
-        echo(json_encode(curl_exec($c)));
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, TRUE);
+        $body = curl_exec($c);
+        echo($body);
+        $httpcode = curl_getinfo($c, CURLINFO_HTTP_CODE);
         curl_close($c);
     }
 
@@ -30,7 +33,34 @@ class sharedBoard {
         global $endpoint, $federatedUser, $federatedKey;
         $thread = $_POST['replyTo'] ?? $_POST['thread'] ?? 0;
         $thread = intval($thread ?? 0);
-        if(!isset($_POST['content'])) { echo 'Err: add content to sharedboard post.'; exit; }
+        $auth_token = null;
+        if(!isset($_POST['content'])) { echo 'Cyberland Server Error: add content to sharedboard post.'; exit; }
+        
+        else { // get authoriation token
+            $auth_payload = array(
+                'username'  =>  $federatedUser,
+                'password'  =>  $federatedKey,
+            );
+            $cauth = curl_init();
+            $auth_url = $endpoint . '/api/v1/auth/';
+            curl_setopt($cauth, CURLOPT_URL, $auth_url);
+            curl_setopt($cauth, CURLOPT_POST, TRUE);                
+            curl_setopt($cauth, CURLOPT_POSTFIELDS, $auth_payload);
+            curl_setopt($cauth, CURLOPT_RETURNTRANSFER, TRUE);
+            $r = json_decode(curl_exec($cauth));
+
+            $httpc = curl_getinfo($cauth, CURLINFO_HTTP_CODE);
+            if ($httpc == 200) { 
+                $auth_token = $r->jwt;
+            } elseif ($httpc == 401) {
+                echo "Cyberland Server Error: This instance (". $_SERVER['HTTP_HOST'] .") does not have an authorized account with api.cyberland2.com.  Open an issue at https://github.com/sailboat-anon/api/issues to request your cyberland server to be federated!.\n";
+                exit;
+            } else {
+                echo "Cyberland Server Error:  Uncaught HTTP Error $httpc \n";
+                exit;
+            }
+            curl_close($cauth);
+        }
 
         $payload = array(
             'replyTo'   =>  $thread,
@@ -38,57 +68,33 @@ class sharedBoard {
         );
         $url = $endpoint . '/s/';
         $c = curl_init();
+
+        $hdr = array('Authorization: Bearer '.$auth_token);
         curl_setopt($c, CURLOPT_URL, $url);
-        curl_setopt($c, CURLOPT_POST, TRUE);
+        curl_setopt($c, CURLOPT_POST, TRUE);   
         curl_setopt($c, CURLOPT_POSTFIELDS, $payload);
-        curl_exec($c);
+        curl_setopt($c, CURLOPT_HTTPHEADER, $hdr);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, TRUE);
+        $resp = json_decode(curl_exec($c));
         $httpcode = curl_getinfo($c, CURLINFO_HTTP_CODE);
         curl_close($c);
 
-        switch ($httpcode) {
-            case 200:
-                header('HTTP/1.1 200 OK', TRUE, 429);
-                break;
-            case 401:
-                header('HTTP/1.1 401 Unauthorized', TRUE, 401); 
-                function() {
-                    global $endpoint;
-                    //api.cyberland2.club/api/v1/auth -d 'username=sba&password=ycLPyE#cLL$wu8WrJ)R~86REecn'
-                    $auth_payload = array(
-                        'username'  =>  $federatedUser,
-                        'password'  =>  $federatedKey,
-                    );
-                    $c = curl_init();
-                    $url = $endpoint . '/api/v1/auth/';
-                    curl_setopt($c, CURLOPT_URL, $endpoint);
-                    curl_setopt($c, CURLOPT_POST, TRUE);
-                    curl_setopt($c, CURLOPT_POSTFIELDS, $auth_payload);
-                    curl_exec($c);
-                    $httpc = curl_getinfo($c, CURLINFO_HTTP_CODE);
-                    curl_close($c);
-                    if ($httpc == 200) { // re-post/get
-                        $this->post();
-                    } elseif ($httpc == 401) {
-                        echo "Cyberland Server Error: You do not have an authorized account with api.cyberland2.com.  Contact sailboatanon@protonmail DOT com to get federated credentials.";
-                        exit;
-                    } else { header('HTTP/1.1 $httpcode', TRUE, $httpcode); }
-                };
-                break;
-            case 404:
-                header('HTTP/1.1 404 Not Found', TRUE, 404);
-                break;
-            case 500:
-                header('HTTP/1.1 500 Internal Server Error', TRUE, 500);
-                break;
-            case 400:
-                header('HTTP/1.1 400 Bad Request', TRUE, 400);
-                break;
-            case 406:
-                header('HTTP/1.1 406 Not Acceptable', TRUE, 406);
-                break;
-            default:
-                header('HTTP/1.1 $httpcode', TRUE, $httpcode);
-                break;
+        // context on the logic below
+        // cannot resend headers (401 Unauthorized, for example); technically the cyberland server gives a 200 (request from client successful)
+        // however api.cyberland2.club might be giving a different response code, so we will serve that in a JSON response
+        if ($httpcode == 200) {
+            echo(json_encode(array(
+                'code'      =>  200,
+                'msg'       =>  'HTTP/1.1 200 OK',
+                'results'   =>  $body,
+            )));
+        }
+        elseif ($httpcode == 401) {          
+            echo(json_encode(array(
+                'code'      =>  401,
+                'msg'       =>  'HTTP/1.1 401 Unauthorized',
+                'results'   =>  null,
+            )));
         }
     }
 }
