57ccb8f0151d89e2135d2a6a9a5b0c812774e212
sailboatAnon <devnull>
2020-04-26 13:16:37 -0700

    no message

--- cyberland.php
+++ cyberland.php
@@ -12,16 +12,6 @@ $port       = $db_config["port"];
 
 use sailboats\sanitizeText;
 
-function getRequestor(){       
-     if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)){
-            return  $_SERVER["HTTP_X_FORWARDED_FOR"];  
-     }else if (array_key_exists('REMOTE_ADDR', $_SERVER)) { 
-            return $_SERVER["REMOTE_ADDR"]; 
-     }else if (array_key_exists('HTTP_CLIENT_IP', $_SERVER)) {
-            return $_SERVER["HTTP_CLIENT_IP"]; 
-     } 
-}
-
 function post(string $board): void
 {
     global $servername;
@@ -36,7 +26,7 @@ function post(string $board): void
         exit;
     }*/
     $rl = new RateLimit();
-    $st = $rl->getSleepTime(getRequestor());
+    $st = $rl->getSleepTime($_SERVER["REMOTE_ADDR"]);
 
     $sanitize = new sanitizeText();
 
